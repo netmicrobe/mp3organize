d8e1d35e4260948370c7defbe66f10a6d490da28
speedio <njnu_wy@126.com>
2019-02-02 16:44:44 +0800

    接受 文件 / 文件夹 参数，对于文件夹参数遍历所有子目录的 mp3 文件。目前 -r 参数开关无效，始终遍历子目录。

--- mp3organize.go
+++ mp3organize.go
@@ -2,20 +2,84 @@ package main
 
 import (
   "fmt"
-  "log"
+//  "log"
+  "flag"
+  "os"
+  "path/filepath"
+  "regexp"
 
   "github.com/bogem/id3v2"
 )
 
 func main() {
-  // Open file and parse tag in it.
-  tag, err := id3v2.Open("Out of My Hands.mp3", id3v2.Options{Parse: true})
+  recursivePtr := flag.Bool("r", false, "recursive in sub-folders")
+  flag.Parse()
+  target := flag.Arg(0) // 文件，或者文件夹
+  if target == "" {
+    fmt.Println("错误！需要指明文件 或 文件夹的路径！")
+    return
+  }
+  
+  fmt.Println(*recursivePtr)
+  fmt.Println(target)
+
+  // 判断是 文件 还是 文件夹
+  isDir, err := IsDirectory(target)
   if err != nil {
-     log.Fatal("Error while opening mp3 file: ", err)
+    fmt.Println(err)
+    return
+  }
+  if isDir {
+    // do directory stuff
+    fmt.Println("directory : ", target)
+    err := filepath.Walk(target,
+      func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if !info.IsDir() {
+          fmt.Println(path, info.Size())
+          r, err := regexp.MatchString(".mp3", info.Name())
+          if err == nil && r {
+            renamemp3(path)
+          }
+        }
+        return nil
+    })
+    if err != nil {
+        fmt.Println(err)
+    }
+  } else {
+    // do file stuff
+    fmt.Println("file : ", target)
+    renamemp3(target)
+  }
+
+}
+
+func IsDirectory(path string) (bool, error) {
+  fileInfo, err := os.Stat(path)
+  if err != nil{
+    return false, err
+  }
+  return fileInfo.IsDir(), err
+}
+
+func renamemp3(mp3fi string) {
+  tag, err := id3v2.Open(mp3fi, id3v2.Options{Parse: true})
+  if err != nil {
+     fmt.Println("Error while opening mp3 file: ", err)
   }
   defer tag.Close()
 
   // Read frames.
-  fmt.Println(tag.Artist())
-  fmt.Println(tag.Title())
+  fmt.Println("    TRY TO RENAME : ", tag.Artist(), "-", tag.Title())
 }
+
+
+
+
+
+
+
+
